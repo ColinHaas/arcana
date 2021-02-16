400be097fa8c96ba5e2f3da6a0b947c20a9797ef
Colin Haas <colinhaas@gmail.com>
2021-02-15 23:33:58 -0800

    Remove external libraries from source control

--- client/photon/lib/speaker/examples/playFromFlash/playFromFlash.ino
+++
@@ -1,9 +0,0 @@
-#include "speaker.h"        
-#include "sound.h"
-
-const int audioFrequency = 22050; // Hz
-
-Speaker speaker((uint16_t*)sound, sizeof(sound) / sizeof(sound[0]));
-void setup() {
-  speaker.begin(audioFrequency);
-}
